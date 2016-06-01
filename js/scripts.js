f898db9b88d85e4f97437586f9e7a42b9e174fb8
Julia Dickey and Katarina Tuttle <julia@>
2016-06-01 14:36:14 -0700

    working input fields and thank you message added

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,21 @@ var fullName = function(firstName, lastName) {
 };
 
 var customersAddress = function(streetAddress, zipCode, state) {
-  return streetAddress + zipCode + state;
+  return streetAddress + " " + zipCode + ", " + state;
 };
 
 // var thankYou = function()
 
 $(document).ready(function() {
-  $("form#name").submit(function(event) {
+  $("form#orderForm").submit(function(event) {
     event.preventDefault();
     var firstName = $("#firstName").val();
     var lastName = $("#lastName").val();
     var resultName = fullName(firstName, lastName);
-    $("#output").text(resultName);
-  });
-// });
-
-// $(document).ready(function() {
-  $("form#address").submit(function(event) {
-    event.preventDefault();
     var streetAddress = $("#streetAddress").val();
     var zipCode = $("#zipCode").val();
     var state = $("#state").val();
-    var resultAddress= customersAddress(streetAddress, zipCode, state);
-    $("#output").text(resultAddress);
+    var resultAddress = customersAddress(streetAddress, zipCode, state);
+    $("#output").text("Thank you for your order, " + firstName + "." + "We will deliver it to " + resultAddress);
   });
 });
